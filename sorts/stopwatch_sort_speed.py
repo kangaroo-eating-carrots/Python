56a5d6671871ef8384b5469658abf2fce101804c
Jeonghyun Min <20122791@tafe.wa.edu.au>
2024-11-04 07:10:50 +0800

    Update: initial start

---
+++ sorts/stopwatch_sort_speed.py
@@ -0,0 +1,33 @@
+# from bubble_sort import bubble_sort_iterative
+# from slowsort import slowsort
+# from binary_insertion_sort import binary_insertion_sort
+# from tree_sort import tree_sort
+# from selection_sort import selection_sort
+# from quick_sort import quick_sort
+
+"""
+One of reasons why learning about algorithm is
+that algorithm can affect the efficiency of the program.
+This function, stop_watch_sort_speed is used to show
+how long each sort algorithm takes to complete a sorting process.
+"""
+
+"""
+This function will be returned list of sorting results including the name,
+whether to sort numbers properly
+and millisecond which is taken to complete the sorting process.
+"""
+
+"""
+여기부터 내 메모
+function Para: (솔트 넘버(5000 이상 추천), [알고리즘 리스트])
+숫자 아니면 return
+0 ~ 솔트넘버*2 에서 솔트넘버만큼 랜덤 초이스
+numbers.sort() 해서 정렬 (나중에 결과랑 비교  ==)
+"""
+
+"""
+time.time()을 활용해서 시간 측정
+다음을 return
+[종류, 솔트숫자, true/false, 밀리초]
+"""
